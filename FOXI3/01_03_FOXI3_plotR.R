6d3cc8267299307f0724d426806c8c94c6f8652b
Annamika Konkola <ak308@rice.edu>
2026-02-15 19:49:18 -0600

    Create 01_03_FOXI3_plotR.R

---
+++ FOXI3/01_03_FOXI3_plotR.R
@@ -0,0 +1,37 @@
+Before R:
+
+- Open the "FOXI3_plink_finalcoded.vcf" as a text edit file
+- copy the inside and paste into a Microsoft Excel file
+- delete the first few rows (the first row should be where all the dogs are listed)
+- delete the 0 on the first sample to Affenpinscher01 and the last sample to YorkshireTerrier77
+- save as a csv file and upload it to your R studio environment
+
+In R:
+
+library (ggplot2)
+library(dplyr)
+library (tidyverse)
+
+### Open the vcf file on your computer and save it as a csv file: 
+
+setwd("~/Downloads")
+dat_hap_FOXI3 <- read.csv("FOXI3_plink_finalcoded.csv", header=TRUE)
+
+# organize the dataset 
+data_long_FOXI3 <- gather(dat_hap_FOXI3, Sample, Allele, Affenpinscher01:YorkshireTerrier77, factor_key=TRUE)
+
+#Make my variables in factor format
+data_long_FOXI3$POS <- as.factor(data_long_FOXI3$POS)
+data_long_FOXI3$Allele <- as.factor(data_long_FOXI3$Allele)
+
+#Plot with geom_tile entire FOXI3 gene
+p1 <- ggplot(data_long_FOXI3,aes(x=POS,y=Sample,fill=Allele))+
+      geom_tile() 
+ p2 <- p1 + scale_fill_manual(values = c("lightblue", "orange", "red", "gray", "black")) 
+p2
+
+# light blue is homozygous reference allele
+# orange is heterozygous
+# red is homozygous alternative allele
+
+ggsave("FOXI3_gene.tiff", width=10,height=3)
