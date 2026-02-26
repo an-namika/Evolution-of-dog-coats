20a7472e67fb4b4a30eb696afdca1a47c629476b
Annamika Konkola <ak308@rice.edu>
2026-02-26 16:47:00 -0600

    Create 02_02_KRT25_runFST.sh

---
+++ KRT25/02_02_KRT25_runFST.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=12:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/ak308_fstscan_KRT25.out 
+#SBATCH --error=/home/ak308/Bio310/output/ak308_fstscan_KRT25.err
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/ak308/Bio310_Dog/Results/KRT25_filtered.recode.vcf
+dogpop_curly=/scratch/ak308/Bio310_Dog/Files/KRT71_curlydogs.txt
+dogpop_noncurly=/scratch/ak308/Bio310_Dog/Files/KRT71_noncurlydogs.txt
+
+vcftools --vcf ${VCF} \
+--weir-fst-pop ${dogpop_curly} \
+--weir-fst-pop ${dogpop_noncurly} \
+--out /scratch/ak308/Bio310_Dog/Results/KRT25_filtered_fst_results
+
+# #### used the KRT71 curly/noncurly dog files:
+
+/scratch/ak308/Bio310_Dog/Files/KRT71_curlydogs.txt
+/scratch/ak308/Bio310_Dog/Files/KRT71_noncurlydogs.txt
