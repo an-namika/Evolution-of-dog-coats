87152402cb85d26a7b1a520e43dd56c3274fc827
Annamika Konkola <ak308@rice.edu>
2026-02-26 16:47:59 -0600

    Update 02_02_FOXI3_runFST.sh

--- FOXI3/02_02_FOXI3_runFST.sh
+++ FOXI3/02_02_FOXI3_runFST.sh
@@ -19,3 +19,13 @@ vcftools --vcf ${VCF} \
 --weir-fst-pop ${dogpop_curly} \
 --weir-fst-pop ${dogpop_noncurly} \
 --out /scratch/ak308/Bio310_Dog/Results/FOXI3_filtered_fst_results
+
+
+## Make the dog population files 
+touch /scratch/ak308/Bio310_Dog/Files/FOXI3_hairlessdogs.txt
+# copy and paste the names of the dogs in the VCF file that have curly hair 
+touch /scratch/ak308/Bio310_Dog/Files/FOXI3_nonhairlessdogs.txt
+# copy and paste the names of the dogs in the VCF file that do not have curly hair 
+
+# for these population files, you would copy and paste the dog individuals that have a specific phenotype, such as 
+# IMPORTANT: by using the "Sample_Name" column
