86f9f4ac430395f8e5e2d7b67a9fb12a044252fe
Annamika Konkola <ak308@rice.edu>
2026-02-26 00:35:39 -0600

    Update 01_01_FGF5_subset_genes.sh

--- FGF5/01_01_FGF5_subset_genes.sh
+++ FGF5/01_01_FGF5_subset_genes.sh
@@ -1,1 +1,39 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=12:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/vcfselect.out 
+#SBATCH --error=/home/ak308/Bio310/output/vcfselect.err
 
+# Grab the specific gene from the large VCF
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/lh106/SharedData/1697canidVCF/g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
+BEDIN=/home/ak308/Bio310/Files/bedfiles/FGF5.bed
+OUT=/scratch/ak308/Bio310_Dog/Results/FGF5_vcf
+
+vcftools --gzvcf ${VCF} --bed  ${BEDIN} --recode --recode-INFO-all --out ${OUT}
+
+#########################
+#How to make a bed file #
+#########################
+
+cd /home/ak308/Bio310/Files/bedfiles
+mkdir bedfiles
+touch FGF5.bed
+vim FGF5.bed
+
+#press i to edit
+# paste below, make sure there are tabs between! 
+chrom   chromStart      chromEnd
+chr32   4508217        4528915
+
+# to exit
+:wq
+
+#Copy and paste the gene location with tabs between
