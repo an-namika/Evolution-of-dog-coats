6f59b2b41b921d6746702a515c4a6324671c4c7b
Annamika Konkola <ak308@rice.edu>
2026-02-26 16:44:55 -0600

    Create 02_02_KANK2_runFST.sh

---
+++ KANK2/02_02_KANK2_runFST.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=12:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/ak308_fstscan_KANK2.out 
+#SBATCH --error=/home/ak308/Bio310/output/ak308_fstscan_KANK2.err
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/ak308/Bio310_Dog/Results/KANK2_filtered.recode.vcf
+dogpop_curly=/scratch/ak308/Bio310_Dog/Files/KANK2_woollydogs.txt
+dogpop_noncurly=/scratch/ak308/Bio310_Dog/Files/KANK2_nonwoollydogs.txt
+
+vcftools --vcf ${VCF} \
+--weir-fst-pop ${dogpop_curly} \
+--weir-fst-pop ${dogpop_noncurly} \
+--out /scratch/ak308/Bio310_Dog/Results/KANK2_filtered_fst_results
