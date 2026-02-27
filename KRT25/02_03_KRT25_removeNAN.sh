77656b24fd33c157bc50e46f5f70116c71d23185
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:18:45 -0600

    Create 02_03_KRT25_removeNAN.sh

---
+++ KRT25/02_03_KRT25_removeNAN.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/removeNAN_KRT25.out 
+#SBATCH --error=/home/ak308/Bio310/output/removeNAN_KRT25.err
+
+FILE=/scratch/ak308/Bio310_Dog/Results/KRT25_filtered_fst_results.weir.fst
+DIR=/scratch/ak308/Bio310_Dog/Results/
+
+sed '/NaN/d' ${FILE} > ${DIR}/KRT25_filtered_fst_results_noNAN.weir.fst
