657058d26da60adb2c621da4d6f6b9b38e31870e
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:36:30 -0600

    Create 02_05_KANK2_slidingwindowFST.sh

---
+++ KANK2/02_05_KANK2_slidingwindowFST.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/ak308_fstscan_window_KANK2.out 
+#SBATCH --error=/home/ak308/Bio310/output/ak308_fstscan_window_KANK2.err
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/ak308/Bio310_Dog/Results/KANK2_filtered.recode.vcf
+dogpop_curly=/scratch/ak308/Bio310_Dog/Files/KANK2_woollydogs.txt
+dogpop_noncurly=/scratch/ak308/Bio310_Dog/Files/KANK2_nonwoollydogs.txt
+OUT=/scratch/ak308/Bio310_Dog/Results
+
+vcftools --vcf ${VCF} \
+--fst-window-size 2500 \
+--fst-window-step 1000 \
+--weir-fst-pop ${dogpop_curly} \
+--weir-fst-pop ${dogpop_noncurly} \
+--out ${OUT}/KANK2_filtered_fst_results_2.5kbwindow_1kbstep
+
+## Make the dog population files 
+# touch /scratch/ak308/Bio310_Dog/Files/KANK2_woollydogs.txt
+# copy and paste the names of the dogs in the VCF file that have woolly coats
+# touch /scratch/ak308/Bio310_Dog/Files/KANK2_nonwoollydogs.txt
+# copy and paste the names of the dogs in the VCf file that do not have woolly coats
