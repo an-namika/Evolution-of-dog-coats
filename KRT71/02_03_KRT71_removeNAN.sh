32285a8e99a346b78242636abcb707cd42cb30de
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:14:20 -0600

    Update 02_03_KRT71_removeNAN.sh

--- KRT71/02_03_KRT71_removeNAN.sh
+++ KRT71/02_03_KRT71_removeNAN.sh
@@ -1,3 +1,27 @@
+## in the end, this was also promising........
+
+
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/removeNAN.out 
+#SBATCH --error=/home/ak308/Bio310/output/removeNAN.err
+
+FILE=/scratch/ak308/Bio310_Dog/Results/KRT71_filtered_fst_results.weir.fst
+DIR=/scratch/ak308/Bio310_Dog/Results/
+
+sed '/NaN/d' ${FILE} > ${DIR}/KRT71_filtered_fst_results_noNAN.weir.fst
+
+
+----------
+
+
+
+
 # INSTEAD of running this script:::: 
 
 cd /scratch/ak308/Bio310_Dog/Results
