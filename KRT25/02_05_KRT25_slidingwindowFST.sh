083ea6c760c14a26650882a0965ceb52a60fff32
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:33:07 -0600

    Create 02_05_KRT25_slidingwindowFST.sh

---
+++ KRT25/02_05_KRT25_slidingwindowFST.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/ak308_fstscan_window_KRT25.out 
+#SBATCH --error=/home/ak308/Bio310/output/ak308_fstscan_window_KRT25.err
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/ak308/Bio310_Dog/Results/KRT25_filtered.recode.vcf
+dogpop_curly=/scratch/ak308/Bio310_Dog/Files/KRT71_curlydogs.txt
+dogpop_noncurly=/scratch/ak308/Bio310_Dog/Files/KRT71_noncurlydogs.txt
+OUT=/scratch/ak308/Bio310_Dog/Results
+
+vcftools --vcf ${VCF} \
+--fst-window-size 2500 \
+--fst-window-step 1000 \
+--weir-fst-pop ${dogpop_curly} \
+--weir-fst-pop ${dogpop_noncurly} \
+--out ${OUT}/KRT25_filtered_fst_results_2.5kbwindow_1kbstep
+
+## Make the dog population files 
+# touch /scratch/ak308/Bio310_Dog/Files/KRT71_curlydogs.txt
+# copy and paste the names of the dogs in the VCF file that have curly hair 
+# touch /scratch/ak308/Bio310_Dog/Files/KRT71_noncurlydogs.txt
+# copy and paste the names of the dogs in the VCf file that do not have curly hair 
