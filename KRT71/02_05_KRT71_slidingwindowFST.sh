5769c73748fc8a1e0de38397ffbb5dcc34863c93
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:22:21 -0600

    Update 02_05_KRT71_slidingwindowFST.sh

--- KRT71/02_05_KRT71_slidingwindowFST.sh
+++ KRT71/02_05_KRT71_slidingwindowFST.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1 
 #SBATCH --mem-per-cpu=1000m 
 #SBATCH --time=00:30:00 
-#SBATCH --output=/home/ak308/Bio310/output/ak308_fstscan.out 
-#SBATCH --error=/home/ak308/Bio310/output/ak308_fstscan.err
+#SBATCH --output=/home/ak308/Bio310/output/ak308_fstscan_window_KRT71.out 
+#SBATCH --error=/home/ak308/Bio310/output/ak308_fstscan_window_KRT71.err
 
 module load GCC/13.3.0
 module load VCFtools/0.1.17
@@ -21,12 +21,12 @@ vcftools --vcf ${VCF} \
 --fst-window-step 1000 \
 --weir-fst-pop ${dogpop_curly} \
 --weir-fst-pop ${dogpop_noncurly} \
---out ${OUT}/KRT71_filtered_fst_results_5kbwindow_2.5kbstep
+--out ${OUT}/KRT71_filtered_fst_results_2.5kbwindow_1kbstep
 
 ## Make the dog population files 
-touch /scratch/ak308/Bio310_Dog/Files/KRT71_curlydogs.txt
+# touch /scratch/ak308/Bio310_Dog/Files/KRT71_curlydogs.txt
 # copy and paste the names of the dogs in the VCF file that have curly hair 
-touch /scratch/ak308/Bio310_Dog/Files/KRT71_noncurlydogs.txt
+# touch /scratch/ak308/Bio310_Dog/Files/KRT71_noncurlydogs.txt
 # copy and paste the names of the dogs in the VCf file that do not have curly hair 
 
-# A window based Fst will calculate the average Fst within a 5,000bp segment of the genome. This can help reduce the noise of calculating Fst for every variant, it kind of smooths out the variation, which sometimes can be helpful to see different small genomic regions that are differienitated.  
+# A window based Fst will calculate the average Fst within a 2,500bp segment of the genome. This can help reduce the noise of calculating Fst for every variant, it kind of smooths out the variation, which sometimes can be helpful to see different small genomic regions that are differienitated.
