00910cc1e1e250759f0401abb9cd845add298e67
Annamika Konkola <ak308@rice.edu>
2026-02-27 09:18:23 -0600

    Update 02_04_KRT25_checkresults.R

--- KRT25/02_04_KRT25_checkresults.R
+++ KRT25/02_04_KRT25_checkresults.R
@@ -14,9 +14,9 @@ Are there any variants that have a high Fst??? (>0.8?)
 #Next you can save this dataset as a .csv and upload it to R studio 
 
 library(ggplot2)
-dat <-read.csv ("KRT25_filtered_fst_results_noNAN.weir.csv", header=TRUE)
+dat_KRT25fst <-read.csv ("KRT25_filtered_fst_results_noNAN.weir.csv", header=TRUE)
 
-ggplot(data = dat, aes(x = POS, y = WEIR_AND_COCKERHAM_FST)) +
+ggplot(data = dat_KRT25fst, aes(x = POS, y = WEIR_AND_COCKERHAM_FST)) +
   geom_point()
 
 # Do we see a candidate variant?!
