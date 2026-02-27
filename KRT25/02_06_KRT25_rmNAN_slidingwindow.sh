6f399cbbd1d83b4f9bf26877027f29d2293e3033
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:32:21 -0600

    Create 02_06_KRT25_rmNAN_slidingwindow.sh

---
+++ KRT25/02_06_KRT25_rmNAN_slidingwindow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/removeNAN_window_KRT25.out 
+#SBATCH --error=/home/ak308/Bio310/output/removeNAN_window_KRT25.err
+
+FILE=/scratch/ak308/Bio310_Dog/Results/KRT25_filtered_fst_results_2.5kbwindow_1kbstep.windowed.weir.fst
+DIR=/scratch/ak308/Bio310_Dog/Results/
+
+sed '/NaN/d' ${FILE} > ${DIR}/KRT25_filtered_fst_results_2.5kbwindow_1kbstep_noNAN.windowed.weir.fst
