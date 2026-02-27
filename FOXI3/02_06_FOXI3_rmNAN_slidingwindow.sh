5fcc9bb0db295e8995c36d104c647145393be784
Annamika Konkola <ak308@rice.edu>
2026-02-26 21:33:33 -0600

    Create 02_06_FOXI3_rmNAN_slidingwindow.sh

---
+++ FOXI3/02_06_FOXI3_rmNAN_slidingwindow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/removeNAN_window_FOXI3.out 
+#SBATCH --error=/home/ak308/Bio310/output/removeNAN_window_FOXI3.err
+
+FILE=/scratch/ak308/Bio310_Dog/Results/FOXI3_filtered_fst_results_2.5kbwindow_1kbstep.windowed.weir.fst
+DIR=/scratch/ak308/Bio310_Dog/Results/
+
+sed '/NaN/d' ${FILE} > ${DIR}/FOXI3_filtered_fst_results_2.5kbwindow_1kbstep_noNAN.windowed.weir.fst
