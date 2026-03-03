3c51fb2725cfbb4a37685eb4b2c7b4b37aa07643
Annamika Konkola <ak308@rice.edu>
2026-03-02 21:59:02 -0600

    Create x_FOXI3_checkResults.R

---
+++ FOXI3/restricted/x_FOXI3_checkResults.R
@@ -0,0 +1,10 @@
+scp ak308@nots.rice.edu:/scratch/ak308/Bio310_Dog/Results/FOXI3_filtered_fst_results_mutset_noNAN.weir.fst ~/Desktop/
+
+
+library(ggplot2)
+
+dat <- read.csv("FOXI3_filtered_fst_results_mutset_noNAN.weir.csv", header=TRUE)
+
+ggplot(data = dat, aes(x = POS, y = WEIR_AND_COCKERHAM_FST)) +
+  geom_point()
+
