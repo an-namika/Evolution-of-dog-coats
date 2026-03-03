187a2e81cfe428dc172dc04bf14c46ee2f594f2a
Annamika Konkola <ak308@rice.edu>
2026-03-02 21:09:37 -0600

    Create 02_07_FOXI3_swFSTresults.R

---
+++ FOXI3/02_07_FOXI3_swFSTresults.R
@@ -0,0 +1,10 @@
+scp ak308@nots.rice.edu:/scratch/ak308/Bio310_Dog/Results/FOXI3_filtered_fst_results_2.5kbwindow_1kbstep_noNAN.windowed.weir.fst ~/Desktop/
+
+library(ggplot2)
+
+dat <- read.csv("FOXI3_filtered_fst_results_2.5kbwindow_1kbstep_noNAN.weir.fst.csv", header=TRUE)
+
+dat$MID <- (dat$BIN_START + dat$BIN_END)/2
+
+ggplot(data = dat, aes(x = MID, y = WEIGHTED_FST)) +
+  geom_point()
