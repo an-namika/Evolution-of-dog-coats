ebefbb0029374d14ba282c5890de6f9c6e602918
Annamika Konkola <ak308@rice.edu>
2026-03-11 23:20:18 -0500

    Create 01_modernADRB1_24870184.sh

---
+++ ancient dogs/01_modernADRB1_24870184.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=12:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/vcfselect.out
+#SBATCH --error=/home/ak308/Bio310/output/vcfselect.err
+
+# Grab the specific gene from the large VCF
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/lh106/SharedData/1697canidVCF/g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
+OUT=/scratch/ak308/Bio310_Dog/Results/modernADRB1_24870184_vcf_ancientcanids
+
+vcftools --gzvcf ${VCF} --chr chr28 --from-bp 24870184 --to-bp 24870184 --recode --recode-INFO-all --out ${OUT}
+
+
+
+scp -r ak308@nots.rice.edu:/scratch/ak308/Bio310_Dog/Results/modernADRB1_24870184_vcf_ancientcanids.recode.vcf ~/Desktop
+
+/scratch/lh106/Dog_Selection/Fstscan_March52026/longhair_vcf_ancientcanids.recode.vcf
