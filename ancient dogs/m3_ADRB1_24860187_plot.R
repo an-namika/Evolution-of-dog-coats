2eb66650da44727e3b227f816b6eacb3d99fb48f
Annamika Konkola <ak308@rice.edu>
2026-03-25 17:17:01 -0500

    Create m3_ADRB1_24860187_plot.R

---
+++ ancient dogs/m3_ADRB1_24860187_plot.R
@@ -0,0 +1,42 @@
+## =========================
+## 4) ADRB1 24860187 MODERN
+## =========================
+
+## Before R: 
+
+- Open the "modernADRB1_24860187_vcf_ancientcanids_finalcoded.vcf" as a text edit file
+- copy the inside and paste into a Microsoft Excel file 
+- delete the first four rows (the first row should be where all the dogs are listed)
+- delete the 0 on the first sample to Affenpinscher01_33500_A and the last sample to Wolf95_B
+- save as a csv file and upload it to your R studio environment
+
+## in R: 
+
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+#Choose the modern variant 
+
+### Open the vcf file on your computer and save it as a csv file: 
+
+dat_hap <- read.csv("modernADRB1_24860187_vcf_ancientcanids_finalcoded.csv", header=TRUE)
+
+# organize the dataset 
+data_long <- gather(dat_hap, Sample, Allele, Affenpinscher01_33500_A:Wolf95_B, factor_key=TRUE)
+
+#Make my variables in factor format
+data_long$POS <- as.factor(data_long$POS)
+data_long$Allele <- as.factor(data_long$Allele)
+
+#Plot with geom_tile entire TSHR gene
+p1 <- ggplot(data_long, aes(x=POS, y=Sample, fill=Allele)) +
+      geom_tile()
+p2 <- p1 + scale_fill_manual(values = c("lightblue", "orange", "red", "gray", "black"))
+p2
+
+# light blue is homozygous reference allele
+# orange is heterozygous
+# red is homozygous alternative allele
+
+ggsave("ADRB1_24860187_gene_modern.tiff", width=10, height=3)
