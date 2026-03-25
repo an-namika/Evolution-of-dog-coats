44ab414b8d7cf63ab8597fc1ea1f12e86006ff35
Annamika Konkola <ak308@rice.edu>
2026-03-25 17:03:45 -0500

    Create aADRB1_24860187_adjustVCF.sh

---
+++ ancient dogs/aADRB1_24860187_adjustVCF.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=combine
+#SBATCH --account=commons
+#SBATCH --partition=commons
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=1000m
+#SBATCH --time=00:30:00
+#SBATCH --output=/home/ak308/Bio310/output/plink.out
+#SBATCH --error=/home/ak308/Bio310/output/plink.err
+
+VCF=/scratch/ak308/Bio310_Dog/Results/ADRB124860187_vcf_ancientcanids.recode.vcf
+OUTBED=/scratch/ak308/Bio310_Dog/Results/ADRB124860187_vcf_ancientcanids_bed
+OUTPLINK=/scratch/ak308/Bio310_Dog/Results/ADRB124860187_vcf_ancientcanids_plink
+OUTVCF_FINAL=/scratch/ak308/Bio310_Dog/Results/ADRB124860187_vcf_ancientcanids_finalcoded
+
+/home/ak308/Software/plink --vcf ${VCF} --make-bed --const-fid --dog --out ${OUTBED}
+/home/ak308/Software/plink --bfile ${OUTBED} --recode --out ${OUTPLINK} --const-fid --dog
+/home/ak308/Software/plink --file ${OUTPLINK} --recode vcf --out ${OUTVCF_FINAL} --const-fid --dog
+
+cd /scratch/ak308/Bio310_Dog/Results/
+
+sed -i 's:0/0:0:g' ADRB124860187_vcf_ancientcanids_finalcoded.vcf
+sed -i 's:0/1:1:g' ADRB124860187_vcf_ancientcanids_finalcoded.vcf
+sed -i 's:1/0:1:g' ADRB124860187_vcf_ancientcanids_finalcoded.vcf
+sed -i 's:1/1:2:g' ADRB124860187_vcf_ancientcanids_finalcoded.vcf
+sed -i 's:\./\.:3:g' ADRB124860187_vcf_ancientcanids_finalcoded.vcf
+
+VCF=/scratch/lh106/SharedData/merged_phased_annotated.Autosomes_ancientdogdingo_FINAL_MAF_0.01_recalibrated_INFO_0.8.vcf.gz
