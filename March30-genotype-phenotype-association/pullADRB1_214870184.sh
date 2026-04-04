17073e1b192769724cb0abc7f5ddf1ce73f240b0
Annamika Konkola <ak308@rice.edu>
2026-04-03 22:14:54 -0500

    Create pullADRB1_214870184.sh

---
+++ March30-genotype-phenotype-association/pullADRB1_214870184.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=12:30:00 
+#SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/vcfselect.out 
+#SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/vcfselect.err
+
+# Grab the specific gene from the large VCF
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/scratch/lh106/SharedData/1697canidVCF/g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
+OUT=/scratch/ak308/Bio310_Dog/Results/ADRB1_24860187_vcf
+
+vcftools --gzvcf ${VCF} --chr chr28 --from-bp 24860187 --to-bp 24860187 --recode --recode-INFO-all --out ${OUT}
