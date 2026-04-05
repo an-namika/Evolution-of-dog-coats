6a2a0d083f3ce8d1a2e36d2d02837698677f9e4d
Annamika Konkola <ak308@rice.edu>
2026-04-04 20:38:27 -0500

    Update adjustADRB1_24870184.sh

--- March30-genotype-phenotype-association/adjustADRB1_24870184.sh
+++ March30-genotype-phenotype-association/adjustADRB1_24870184.sh
@@ -17,7 +17,7 @@ OUTVCF_final=/scratch/ak308/Bio310_Dog/Results/ADRB1_24870184_vcf_finalcoded
 /home/ak308/Software/plink --bfile ${OUTBED} --recode --out ${OUTPLINK} --const-fid --dog
 /home/ak308/Software/plink --file  ${OUTPLINK} --recode vcf --out ${OUTVCF_final} --const-fid --dog
 
-cd /scratch/ak308/Dog_Selection/Results/
+cd /scratch/ak308/Bio310_Dog/Results/
 ##### Convert vcf to coding ######
 sed -i 's:0/0:0:g' ADRB1_24870184_vcf_finalcoded.vcf
 sed -i 's:0/1:1:g' ADRB1_24870184_vcf_finalcoded.vcf
